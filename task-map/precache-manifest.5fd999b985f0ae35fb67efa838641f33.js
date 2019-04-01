64f4000984a723f2904fda30a5d8fb2a6cf8e66e
Dima Lagutin <fsss70178@gmail.com>
2019-04-01 20:47:00 +0400

    add task-map

---
+++ task-map/precache-manifest.5fd999b985f0ae35fb67efa838641f33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "003cbc5bc70a91272e68",
+    "url": "./static/js/main.3ad96f42.chunk.js"
+  },
+  {
+    "revision": "a3c1411aa34e1663b3e3",
+    "url": "./static/js/2.de45fc82.chunk.js"
+  },
+  {
+    "revision": "003cbc5bc70a91272e68",
+    "url": "./static/css/main.259ea584.chunk.css"
+  },
+  {
+    "revision": "127bac728a62fbc8b96c76fdba096bfa",
+    "url": "./index.html"
+  }
+];
