c3b226b97da0f0e0476d3ef882fa2a43f11695c1
Dima Lagutin <fsss70178@gmail.com>
2019-04-05 15:05:00 +0400

    add todo

---
+++ todo/precache-manifest.701fba17dedae7af597d0d06644c6fc2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5bf1212a12c7b9cfebe6",
+    "url": "./static/js/main.159ec0f7.chunk.js"
+  },
+  {
+    "revision": "8fa24cf241198ccda60a",
+    "url": "./static/js/2.224d3194.chunk.js"
+  },
+  {
+    "revision": "9b36b9e4d4626d8c301c07f7259f8677",
+    "url": "./index.html"
+  }
+];
